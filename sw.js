35d911456ffbf01b2d98e6b3224323df1ac516e5
comunity <kangrouf@gmail.com>
2026-07-10 10:20:00 +0700

    sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'booking-cache-v1';
+const CACHE_NAME = 'booking-cache-v2';
 const urlsToCache = ['./', 'index.html', 'manifest.json'];
 
 self.addEventListener('install', event => {
